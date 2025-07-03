832257c5df53bf0a08d958c1bdd9c4b08e08e9cd
tuantu04 <tuantu04@gmail.com>
2025-07-03 19:55:35 +0700

    [be_1] Update return DTO on Login

--- src/main/java/com/javaweb/controller/AuthController.java
+++ src/main/java/com/javaweb/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.javaweb.controller;
 
 
 import com.javaweb.dtos.request.LoginRequest;
+import com.javaweb.dtos.response.admin.UserDTO;
 import com.javaweb.entities.Course;
 import com.javaweb.entities.User;
 import com.javaweb.services.UserService;
@@ -19,13 +20,26 @@ public class AuthController {
     private UserService userService;
 
     @PostMapping("/login")
-    public ResponseEntity<User> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<UserDTO> login(@RequestBody LoginRequest loginRequest) {
         User user = userService.login(loginRequest.getEmail(), loginRequest.getPassword());
         if (user != null && Boolean.TRUE.equals(user.getIsActive())) {
-            return ResponseEntity.ok(user);
+            UserDTO userDTO = convertToDTO(user);
+            return ResponseEntity.ok(userDTO);
         } else {
             return ResponseEntity.noContent().build();
         }
+    }
+
+    private UserDTO convertToDTO(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUserId(user.getUserId());
+        userDTO.setFullName(user.getFullName());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setBirthDate(user.getBirthDate().toString());
+        userDTO.setPassword(user.getPassword());
+        userDTO.setRole(user.getRole());
+        userDTO.setImageUrl(user.getImageUrl());
+        return userDTO;
     }
 
 }
